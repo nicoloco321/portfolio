4764f18f07ffad51e4951cb71df725e050fb418d
Nicholas Buryniuk <nicoloco321@gmail.com>
2025-05-05 02:05:16 -0400

    umami setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from 'dotenv';
+import { fs } from "fs";
+
+const env = dotenv.parse(fs.readFileSync('./src/cred/umami.env'));
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	base: "/portfolio/",
+	define: {
+    'import.meta.env.VITE_UMAMI_WEBSITE_ID': JSON.stringify(env.VITE_UMAMI_WEBSITE_ID),
+  },
 });
